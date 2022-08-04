cfb34986450659b227ea21af40c4a07f3806ec5c
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-08-04 16:29:42 +0200

    update failed test case

--- Current_SamplingFunctionality.cpp
+++ Current_SamplingFunctionality.cpp
@@ -6,22 +6,14 @@ using namespace std;
 
 unsigned int ConsecutiveRanges(int Current_Samples[], int Current_Samples_size) {
 
-    //int Current_Samples_arr_size = sizeof(Current_Samples)/sizeof(Current_Samples[0]);
     unsigned int count = 1;
     int idx; // index
-    cout << "sizeof(Current_Samples) =" << sizeof(Current_Samples) << endl;
-    cout << "sizeof(Current_Samples[0] =" << sizeof(Current_Samples[0]) << endl;
-    cout << "Current_Samples_arr_size =" << Current_Samples_size << endl;
     // check if the current element & previous element step is 1.
     for(idx=0; idx < Current_Samples_size; idx++) {
-        
-        cout << "Current_Samples[idx +1] " << Current_Samples[idx +1] << endl;
-        cout << "Current_Samples[idx ] " << Current_Samples[idx] << endl;
+
         if ((Current_Samples[idx + 1] - Current_Samples[idx]) == 1 ) {
             count = count + 1;
-            cout << "Incremented " << endl;
         }
-
     }
     return count;
 }
@@ -29,8 +21,6 @@ unsigned int ConsecutiveRanges(int Current_Samples[], int Current_Samples_size) 
 bool SampleRangeReadings(int Current_Samples[], int Current_Samples_size, int ReadingsCount)
 {
     unsigned int GetReadingsCount = ConsecutiveRanges(Current_Samples, Current_Samples_size);
-
-    cout << "got count of " << GetReadingsCount << endl;
 
     if(GetReadingsCount == ReadingsCount)
     {
--- Test_Cases_main.cpp
+++ Test_Cases_main.cpp
@@ -24,3 +24,15 @@ TEST_CASE("PASSING TEST CASE 3") {
     int Current_Samples_size = sizeof(Current_Samples)/sizeof(Current_Samples[0]);
     REQUIRE(SampleRangeReadings(Current_Samples, Current_Samples_size, 3) == true);
 }
+
+TEST_CASE("PASSING TEST CASE 3") {
+    int Current_Samples [] = { 1, 4, 6, 7, 8, 10 };
+    int Current_Samples_size = sizeof(Current_Samples)/sizeof(Current_Samples[0]);
+    REQUIRE(SampleRangeReadings(Current_Samples, Current_Samples_size, 3) == true);
+}
+
+TEST_CASE("FAILING TEST CASE 1") {
+    int Current_Samples [] = { 1, 4, 6, 7, 8, 10 };
+    int Current_Samples_size = sizeof(Current_Samples)/sizeof(Current_Samples[0]);
+    REQUIRE(SampleRangeReadings(Current_Samples, Current_Samples_size, 2) == false);
+}
