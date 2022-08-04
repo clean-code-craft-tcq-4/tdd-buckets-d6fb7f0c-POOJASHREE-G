88f0d4e006040984a837b1d6452b446973507cac
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-08-04 16:17:49 +0200

    Update Test_Cases_main.cpp

--- Test_Cases_main.cpp
+++ Test_Cases_main.cpp
@@ -5,24 +5,19 @@
 #include "Current_SamplingFunctionality.hpp"
 using namespace std;
 
-int main() {
 
-    // As a first step, only considering to detect continuous ranges in the samples
-    TEST_CASE("PASSING TEST CASE 1") {
-        int Current_Samples_1 [] = { 4, 5 };
-        REQUIRE(SampleRangeReadings(Current_Samples_1, 2) == true);
-    }
+// As a first step, only considering to detect continuous ranges in the samples
+TEST_CASE("PASSING TEST CASE 1") {
+    int Current_Samples_1 [] = { 4, 5 };
+    REQUIRE(SampleRangeReadings(Current_Samples_1, 2) == true);
+}
 
-    TEST_CASE("PASSING TEST CASE 2") {
-        int Current_Samples_2 [] = { 4, 5, 10 };
-        REQUIRE(SampleRangeReadings(Current_Samples_2, 2) == true);
-    }
+TEST_CASE("PASSING TEST CASE 2") {
+    int Current_Samples_2 [] = { 4, 5, 10 };
+    REQUIRE(SampleRangeReadings(Current_Samples_2, 2) == true);
+}
 
-    TEST_CASE("PASSING TEST CASE 3") {
-        int Current_Samples_3 [] = { 1, 4, 6, 7, 8, 10 };
-        REQUIRE(SampleRangeReadings(Current_Samples_3, 3) == true);
-    }
-
-    return 0;
-
+TEST_CASE("PASSING TEST CASE 3") {
+    int Current_Samples_3 [] = { 1, 4, 6, 7, 8, 10 };
+    REQUIRE(SampleRangeReadings(Current_Samples_3, 3) == true);
 }
