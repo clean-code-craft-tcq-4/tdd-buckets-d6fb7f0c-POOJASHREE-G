1c7ece1ce68c58cd22de50c96e63c154201c72fc
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-08-04 12:54:27 +0200

    Update Current_SamplingFunctionality.cpp

--- Current_SamplingFunctionality.cpp
+++ Current_SamplingFunctionality.cpp
@@ -10,8 +10,7 @@ unsigned int ConsecutiveRanges(int Current_Samples[]) {
     unsigned int count = 1;
     int idx = 1; // index
 
-    // check if the current element is equal to
-    // previous element.
+    // check if the current element & previous element step is 1.
     for(idx=0; idx < Current_Samples_arr_size; idx++) {
 
         if ((Current_Samples[idx] - Current_Samples[idx - 1]) == 1 ) {
@@ -32,8 +31,11 @@ bool SampleRangeReadings(int Current_Samples[], int ReadingsCount)
     {
         return true;
     }
+    else
+    {
+        return false;
+    }
 
-    return false;
 }
 
 
