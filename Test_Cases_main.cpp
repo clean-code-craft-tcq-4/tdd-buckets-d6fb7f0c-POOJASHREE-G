88017988706fb2c2548716a6ce258970bf5550e3
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-08-04 16:07:31 +0200

    Update Test_Cases_main.cpp

--- Test_Cases_main.cpp
+++ Test_Cases_main.cpp
@@ -6,18 +6,25 @@ using namespace std;
 int main() {
 
     // As a first step, only considering to detect continuous ranges in the samples
-    int Current_Samples_1 [] = { 4, 5 };
-    int Current_Samples_2 [] = { 4, 5, 10 };
-    int Current_Samples_3 [] = { 1, 4, 6, 7, 8, 10 };
+    {
+        int Current_Samples_1 [] = { 4, 5 };
+        // Passing Test Case
+        assert(SampleRangeReadings(Current_Samples_1, 2) == true);
+    }
 
-    // Passing Test Case
-    assert(SampleRangeReadings(Current_Samples_1, 2) == true);
+    {
+        int Current_Samples_2 [] = { 4, 5, 10 };
+        // Passing Test Case 
+        assert(SampleRangeReadings(Current_Samples_2, 2) == true);
+    }
 
-    // Passing Test Case 
-    assert(SampleRangeReadings(Current_Samples_2, 2) == true);
 
-    // Failing Test Case
-    assert(SampleRangeReadings(Current_Samples_3, 3) == true);
+    {
+        int Current_Samples_3 [] = { 1, 4, 6, 7, 8, 10 };
+        // Failing Test Case
+        assert(SampleRangeReadings(Current_Samples_3, 3) == true);
+    }
+
 
     return 0;
 
