211802f89d0435acc92a8a175107ac8ec925b75e
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-08-04 16:15:37 +0200

    Update catch framework

--- Test_Cases_main.cpp
+++ Test_Cases_main.cpp
@@ -1,4 +1,6 @@
-#include <assert.h>
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test/catch.hpp"
 #include <iostream>
 #include "Current_SamplingFunctionality.hpp"
 using namespace std;
@@ -6,25 +8,20 @@ using namespace std;
 int main() {
 
     // As a first step, only considering to detect continuous ranges in the samples
-    {
+    TEST_CASE("PASSING TEST CASE 1") {
         int Current_Samples_1 [] = { 4, 5 };
-        // Passing Test Case
-        assert(SampleRangeReadings(Current_Samples_1, 2) == true);
+        REQUIRE(SampleRangeReadings(Current_Samples_1, 2) == true);
     }
 
-    {
+    TEST_CASE("PASSING TEST CASE 2") {
         int Current_Samples_2 [] = { 4, 5, 10 };
-        // Passing Test Case 
-        assert(SampleRangeReadings(Current_Samples_2, 2) == true);
+        REQUIRE(SampleRangeReadings(Current_Samples_2, 2) == true);
     }
 
-
-    {
+    TEST_CASE("PASSING TEST CASE 3") {
         int Current_Samples_3 [] = { 1, 4, 6, 7, 8, 10 };
-        // Failing Test Case
-        assert(SampleRangeReadings(Current_Samples_3, 3) == true);
+        REQUIRE(SampleRangeReadings(Current_Samples_3, 3) == true);
     }
-
 
     return 0;
 
