770ccd36a987a3c116fae3d119ed5bda30e80abc
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-08-24 12:06:34 +0200

    Update Test_Cases_main.cpp

--- Test_Cases_main.cpp
+++ Test_Cases_main.cpp
@@ -34,18 +34,18 @@ TEST_CASE("FAILING TEST CASE 1") {
 TEST_CASE("PASSING TEST CASE 1 ( CONSECUTIVE RANGES )") {
     unsigned int Current_Samples [] = { 3, 4, 6, 7, 8, 10, 11, 12 };
     int Current_Samples_size = sizeof(Current_Samples)/sizeof(Current_Samples[0]);
-    REQUIRE(GetConsecutiveRange(Current_Samples, Current_Samples_size) == ("3 -> 4 , Reading:2 ,6 -> 8 , Reading:3,10 -> 12 , Reading:3"));
+    REQUIRE(GetConsecutiveRange(Current_Samples, Current_Samples_size) == ("3 -> 4 , Reading:2 ,6 -> 8 , Reading:3 ,10 -> 12 , Reading:3 "));
 }
 
 TEST_CASE("PASSING TEST CASE 2 ( CONSECUTIVE RANGES )") {
     unsigned int Current_Samples [] = { 3, 3, 4, 5, 6, 7, 8, 10, 11, 12 };
     int Current_Samples_size = sizeof(Current_Samples)/sizeof(Current_Samples[0]);
-    REQUIRE(GetConsecutiveRange(Current_Samples, Current_Samples_size) == ("4 -> 8 Reading:5, 10 -> 12 Reading:3"));
+    REQUIRE(GetConsecutiveRange(Current_Samples, Current_Samples_size) == ("3 -> 8 , Reading:7 ,10 -> 12 , Reading:3 "));
 
 }
 
 TEST_CASE("PASSING TEST CASE 3 ( CONSECUTIVE RANGES )") {
     unsigned int Current_Samples [] = { 4, 4, 5, 6, 7, 8, 8, 10, 11, 12, 12 };
     int Current_Samples_size = sizeof(Current_Samples)/sizeof(Current_Samples[0]);
-    REQUIRE(GetConsecutiveRange(Current_Samples, Current_Samples_size) == ("0 -> 1 , Reading:1,4 -> 8 , Reading:6,10 -> 12 , Reading:4"));
+    REQUIRE(GetConsecutiveRange(Current_Samples, Current_Samples_size) == ("3 -> 8 , Reading:7 ,10 -> 12 , Reading:4 "));
 }
