f15e2683df88467b7c7c4a8a6c2b33a59766b10b
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-08-31 14:53:00 +0200

    update adc to amp converter

--- ADC_To_AmpConverter.cpp
+++ ADC_To_AmpConverter.cpp
@@ -5,6 +5,6 @@ using namespace std;
 
 float ADCToAmpereConverter(int f_InputADC_Data) {
 
-    return (CONVERSION_SCALE * f_InputADC_Data / MAXIMUM_OFFSET);
+    return ((CONVERSION_SCALE * f_InputADC_Data) / (MAXIMUM_OFFSET)) + (CONVERSION_OFFSET);
 
 }
